6d01e3bffacc8da6b6e2094a020ef489a5649820
Ajoy <ajoy.sojan@quartzy.com>
2012-09-24 12:58:10 -0700

    git to svn python script

---
+++ git2svn.py
@@ -0,0 +1,98 @@
+import os
+
+# read in the commit diffs
+
+# Eg.
+# git diff --name-status 2abc775fae354a0b4deaee1f2eb1df9a7ff573a3 c1f0530a4c02706ef0bc28ec301c268d450f3b47
+# M       apps/frontend/modules/profile/templates/_commonJs.php
+# M       apps/frontend/modules/reagent/actions/addbatchimportreagentAction.class.php
+# M       apps/frontend/modules/reagent/templates/addbatchSuccess.php
+# M       apps/frontend/modules/reagent/templates/batchImportDetailsSuccess.php
+# M       apps/frontend/modules/upload/templates/batchReagentImport2Success.php
+# D		  apps/frontend/modules/upload/actions/batchReagentImport2Action.php
+
+
+# run git log
+# find all commits since last commit
+
+prev_commit_file = open('last_commit.git', "r")
+prev_commit = prev_commit_file.readline()
+prev_commit_file.close()
+
+git_repo = 'git-repo'
+svn_repo = 'svn-repo'
+
+os.chdir( '../'+git_repo)
+
+# prev_commit = 'a3edbde4'
+child = os.popen( "git log --pretty=format:'%h' " + prev_commit + "..")
+output = child.read()
+child.close()
+
+# split output into lines
+commits = output.split('\n')
+latest_commit = commits[0]
+# write commit to file
+
+child = os.popen("git diff --name-status " + prev_commit + " " + latest_commit)
+commitdiff = child.read()
+child.close()
+
+# get the files modified
+print commitdiff
+os.chdir( '../'+svn_repo)
+
+filediff = commitdiff.split('\n')
+
+filenames = ''
+
+for line in filediff:
+	line = line.strip()
+	if line == '':
+		continue
+
+	print line
+	mode, filename = line.split('\t',1)
+	mode = mode.strip()
+	filename = filename.strip()
+
+	if filename == '':
+		continue
+
+	if mode == 'M' or mode == 'A':
+		# file modified
+		# need to be copied
+		if not os.path.exists('../git-repo/' + filename):
+			print 'File Not Found : ', filename
+			continue
+
+		print 'cp ../git-repo/'+filename, filename
+		os.system( 'cp ../git-repo/'+filename + ' ' + filename )
+
+	if mode == 'A':
+		# new file
+		# need to be copied
+		print 'cp ../git-repo/'+filename, filename
+		os.system( 'cp ../git-repo/'+filename + ' ' + filename )
+		print 'svn add ', filename
+		os.system( 'svn add ' + filename)
+
+	if mode == 'D':
+		# file deleted
+		print 'svn rm', filename
+		os.system( 'svn rm ' + filename)
+
+	filenames += ' ' + filename
+
+os.system('svn status')
+
+print 'Latest Commit : ' + latest_commit
+
+# save latest commit to file
+last_commit_file = open('last_commit.git', "w")
+last_commit_file.writelines( latest_commit)
+last_commit_file.close()
+
+print 'done\n'
+print 'svn commit' , filenames, '-m ""'
+
